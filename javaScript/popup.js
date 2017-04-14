fc177614455b78ce11dc584b03b784eed6797be1
Philip Athyal <philip.athyal@yahoo.com>
2017-04-13 22:34:15 -0600

    Cleanup

--- javaScript/popup.js
+++ javaScript/popup.js
@@ -5,44 +5,36 @@
 var selected_Url;
 var bkPage = chrome.extension.getBackgroundPage();
 
-function my_Notification(website){
+function stringEncapsulate(url){
+    return ("*://" + url + "/*");
+}
 
+function my_Notification(website){
     var notification = new Notification("My Minutes",{body: website + " saved to storage, now unavailable"});
     setTimeout(function(){
         notification.close();},
         2000);
 }
 
-function getUrl(callback){
-    chrome.tabs.query({
-        active:true,
-        currentWindow:true
-    },function (tabs){
-        callback(tabs);
-        });
-}
-
-function save_to_Storage(website){
-    chrome.storage.sync.set({'myWebsites':website}, function(){
-        bkPage.get_Url();
-        my_Notification(website.url);
-    });
-}
-
 function clean_URL(url){
+    //Creating an a element to extract hostname
     var a = document.createElement('a');
     a.setAttribute('href',url);
     var hostname = a.hostname;
-    try{
-        var time = document.getElementById("text_field").value;
-        var website = new Website(hostname,time);
-        save_to_Storage(website);
-    }catch(err){
-        if(time == null)
-            alert(err);
-    }
+    
+    my_Notification(hostname);
+    var time = document.getElementById("text_field").value;
+    var website = new Website(stringEncapsulate(hostname),parseInt(time));
+    console.log(website);
+   // bkPage.save_to_Storage(website);
     
     return hostname;
+}
+
+//------------------------------------
+function getDomain(tabs){
+        var url = tabs[0].url;
+        document.getElementById("message").innerHTML = if_defined(url); 
 }
 
 function if_defined(url){
@@ -52,26 +44,29 @@ function if_defined(url){
         return "No Page";
     
 }
-
-function getDomain(tabs){
-        var url = tabs[0].url;
-        //var domain = url.domain;
-        document.getElementById("message").innerHTML = if_defined(url); 
-}
+//-----------------------------------
 
 function saveUrl(){
-        getUrl(getDomain);
+    var time = document.getElementById("text_field").value;
+    //Validating textbox for int
+    if(!isNaN(time)&&time.length>0){
+        chrome.tabs.query({
+        active:true,
+        currentWindow:true
+    },function (tabs){
+        getDomain(tabs);
+        });
+    }
+    else
+       document.getElementById("message").innerHTML = "Invalid entry"; 
 }
 
 window.onload = function(){
-   // var button1 = document.getElementById("button1");
-   // var url_button = document.getElementById("url_button");
-   //button1.addEventListener("click", my_Notification);
     url_button.addEventListener("click", saveUrl);
-}
+};
 
-//-----------
-
+//-----------------
+//Website prototype
 function Website(url, remainingTime){
     this.url = url;
     this.remainingTime = remainingTime;
--- public_html/background.js
+++ public_html/background.js
@@ -4,17 +4,20 @@
  * and open the template in the editor.
  */
 
-var url_list = ["*://9gag.com/*"];
+var url_list = ["*://9gag.com/*","*://www.reddit.com/*"];
 
-function stringEncapsulate(url){
-    return ("*://" + url + "/*");
-}
 
 function get_Url(){
     chrome.storage.sync.get('myWebsites',function(data){
         console.log(data);
        //url_list.push(stringEncapsulate(data.myUrls));
        updateListener();
+    });
+}
+
+function save_to_Storage(website){
+    chrome.storage.sync.set({'myWebsites':website}, function(){
+        get_Url();
     });
 }
 
@@ -29,7 +32,15 @@ function updateListener(){
     }
 }
 
-function blockingListener(){
+function blockingListener(details){
+    //go through url_list
+        //if active
+            //countdowntimer - using remainingTime
+            //if remainingTime < 0 cancel:true
+            //else cancel:false
+        //else stop timer - new remainingTime
+        //cancel:false
+        console.log(details.url);
     return {cancel:true};
 }
 
@@ -39,4 +50,13 @@ chrome.webRequest.onBeforeRequest.addListener(
         {urls: url_list},
         ["blocking"]
 );
-  
+
+chrome.tabs.onActivated.addListener(
+        function(){
+          chrome.tabs.getSelected(null,function(tab){
+              console.log(tab.url);
+        }); 
+});
+
+//--------------------
+
